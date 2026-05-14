ab03eaf1efa7284bf04e526b5071f34499882162
Arailym <optics@app.kz>
2026-05-14 23:43:43 +0500

    Add full content fixture and update build script for automatic deployment

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata core/fixtures/initial_data.json
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('Arailym', 'admin@example.com', 'teacher123')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Arailym').exists() or User.objects.create_superuser('Arailym', 'admin@example.com', 'admin123')"
